eaeb5b10b9ec521c7632efb013fc51b44d042928
jfcaraujo <jfcaraujo99@gmail.com>
2019-11-28 16:39:41 +0000

    New database with classification in places

--- database/questions.sql
+++ database/questions.sql
@@ -1,3 +1,4 @@
--- select * from PLACE ORDER BY idPlace DESC LIMIT 3;
--- select * from PICTURES where idPlace=X;
-select * from PLACE 
+-- select * from PLACE ORDER BY idPlace DESC LIMIT 4; --most recent, limit 4
+-- select * from PLACE ORDER BY classification DESC LIMIT 4; --best classified, limit 4
+select count(RESERVATION.idPlace) from PLACE,RESERVATION WHERE PLACE.idPlace=RESERVATION.idPlace;
+-- select * from PICTURES where idPlace=X; --select pictures from house x
