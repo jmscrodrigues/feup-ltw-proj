30cf7a72866af11a5223fa36ca766f479e75db22
Marantesss <up201706917@fe.up.pt>
2019-12-09 18:06:46 +0000

    Added includes for session

---
+++ includes/session.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+
+    function set_current_user($userId, $username) {
+        $_SESSION['userId'] = $userId;
+        $_SESSION['username'] = $username;
+    }
+
+    function get_user_id() {
+        if(isset($_SESSION['userId'])) {
+            return $_SESSION['userId'];
+        }
+        else {
+            return null;
+        }
+    }
+
+    function get_username() {
+        if(isset($_SESSION['username'])) {
+            return $_SESSION['username'];
+        }
+        else {
+            return null;
+        }
+    }
+?>
