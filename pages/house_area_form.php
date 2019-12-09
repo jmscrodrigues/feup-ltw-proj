fbb62c3645d8e6703dc6a6d48ca0fc8f2c7b2c09
Marantesss <up201706917@fe.up.pt>
2019-12-09 17:57:01 +0000

    Need to redirect

---
+++ pages/house_area_form.php
@@ -0,0 +1,5 @@
+<?php
+    include_once('templates/common/header_alternative.php');
+    include_once('templates/house/house_area_form.php');
+    include_once('templates/common/footer.php');
+?>
---
+++ pages/house_area_rents.php
@@ -0,0 +1,5 @@
+<?php
+    include_once('templates/common/header_alternative.php');
+    include_once('templates/house/house_area_rents.php');
+    include_once('templates/common/footer.php');
+?>
