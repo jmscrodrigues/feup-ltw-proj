2fd1b5bba3ff6a685accf3b9fc7f359f6f97a4ec
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2019-12-12 15:52:11 +0000

    db_user com edit username e password

--- database/db_user.php
+++ database/db_user.php
@@ -46,6 +46,45 @@
         return -1;
         }
     }
+
+    function editPassword($username, $password, $newPassword) {
+        
+        global $dbh;
+
+        $stmt = $dbh->prepare('select * from user where username = ? and password = ?'); 
+        $hashPassword=hash('sha256',$password);
+        $stmt->execute(array($username, $hashPassword));
+        if ($stmt->fetchAll() == false) {
+            print("wrong password!");
+            return -1;
+        } 
+        else {
+            $stmt1 = $dbh->prepare("update user set password = ? where username='$username'");
+            $newHashPassword=hash('sha256',$newPassword);
+            $stmt1->execute(array($newHashPassword));
+            print("updated");
+            return 0;
+        }   
+    }
+
+    function editUsername($username, $newUsername, $password) {
+
+        global $dbh; 
+
+        $stmt = $dbh->prepare('select * from user where username = ? and password = ?'); 
+        $hashPassword=hash('sha256',$password);
+        $stmt->execute(array($username, $hashPassword));
+        if ($stmt->fetchAll() == false) {
+            print("wrong password!");
+            return -3;
+        }   
+        else {
+            $stmt2 = $dbh->prepare("update user set username = ? where username='$username'");
+            $stmt2->execute(array($newUsername));
+            print("updated");
+            return 0;
+        }
+    }
        
     
 ?>
