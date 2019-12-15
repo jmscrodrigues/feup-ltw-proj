1be9a1148ac526ba2204e3236d95c188bda1b249
Marantesss <up201706917@fe.up.pt>
2019-12-15 18:22:06 +0000

    Search page is responsive and search form is complete

--- templates/house_list.php
+++ templates/house_list.php
@@ -10,40 +10,46 @@
   <section id="house-list-search-bar">
     <h3>Search</h3>
     <form>
-    <label>Name
-      <input class="input-form" type="text" name="name" placeholder="House Name">
-    </label>
-    <label>Country
-      <select class="input-form select-form" name="country">
-        <option value="All">All</option>
-        <option value="Portugal">Portugal</option>
-        <option value="Spain">Spain</option>
-        <option value="Italy">Italy</option>
-      </select>
-    </label>
-    <label>City
-      <select class="input-form select-form" name="city">
-        <option value="All">All</option>
-        <option value="Porto">Porto</option>
-        <option value="Lisbon">Lisbon</option>
-        <option value="Aveiro">Aveiro</option>
-      </select>
-    </label>
-    <label>Price
-      <section class="range-slider">
-        <span class="rangeValues"></span>
-        <input class="price-slider" name="minprice" value="0" min="0" max="200" step="1" type="range">
-        <input class="price-slider" name="maxprice" value="200" min="0" max="200" step="1" type="range">
-      </section>
-    </label>
-    <label>Rating
-      <section class="range-slider">
-        <span class="rangeValues"></span>
-        <input class="price-slider" name="minrating" value="0" min="0" max="5" step="0.1" type="range">
-        <input class="price-slider" name="maxrating" value="5" min="0" max="5" step="0.1" type="range">
-      </section>
-    </label>
-  </select>
+      <label>Name
+        <input class="input-form" type="text" name="name" placeholder="House Name">
+      </label>
+      <label>Country
+        <select class="input-form select-form" name="country">
+          <option value="All">All</option>
+          <option value="Portugal">Portugal</option>
+          <option value="Spain">Spain</option>
+          <option value="Italy">Italy</option>
+        </select>
+      </label>
+      <label>City
+        <select class="input-form select-form" name="city">
+          <option value="All">All</option>
+          <option value="Porto">Porto</option>
+          <option value="Lisbon">Lisbon</option>
+          <option value="Aveiro">Aveiro</option>
+        </select>
+      </label>
+      <label>Price
+        <section class="range-slider">
+          <span class="rangeValues"></span>
+          <input class="price-slider" name="minprice" value="0" min="0" max="200" step="1" type="range">
+          <input class="price-slider" name="maxprice" value="200" min="0" max="200" step="1" type="range">
+        </section>
+      </label>
+      <label>Rating
+        <section class="range-slider">
+          <span class="rangeValues"></span>
+          <input class="price-slider" name="minrating" value="0" min="0" max="5" step="0.1" type="range">
+          <input class="price-slider" name="maxrating" value="5" min="0" max="5" step="0.1" type="range">
+        </section>
+      </label>
+      <label>Start Date
+        <input class="input-form" type="date" name="startdate">
+      </label>
+      <label>End Date
+        <input class="input-form" type="date" name="finaldate">
+      </label>
+      <input class="blue-filled-rounded-button" type="submit" value="Search">
     </form>
   </section>
   <article class='house-article-container'>
